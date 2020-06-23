1103284779e960bb25d8100c6e052f1f9f81b54e
walt <waltdevelops@gmail.com>
2020-06-22 22:30:08 -0400

    Text field label can now be changed by button or text field

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -3,10 +3,37 @@
 //
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
-    override func viewDidLoad() {
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Handle the text field's user input through delgate callbacks
+        nameTextField.delegate = self
+    }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField)
+    -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField)
+    {
+        // Read text field
+        mealNameLabel.text = textField.text
+    }
+    
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton)
+    {
+        mealNameLabel.text = "Default text"
     }
 }
